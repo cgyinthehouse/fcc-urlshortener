ed20b3f36bf50f7ff084465588bb46e52c22c54b
cgy <100090733+cgyinthehouse@users.noreply.github.com>
2023-07-28 15:11:48 +0800

    finish project

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { nanoid } from "nanoid";
 
 // Basic Configuration
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 3000;
+const shorturls = {};
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(_req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function(req, res) {
+  const id = nanoid(4);
+  const original_url = req.body.url;
+  
+  const isValidUrl = (url) => {
+    const urlRegex = /^(http)s?:\/\/[^\s/$.?#].[^\s]*$/i;
+    return urlRegex.test(url);
+  };
+  
+  if (isValidUrl(original_url)) {
+    new URL(original_url);
+    shorturls[id] = original_url;
+    res.json({ original_url, short_url: id });
+  } else {
+    res.json({ error: "invalid url" });
+  }
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = req.params.id;
+  if (id in shorturls) return res.redirect(shorturls[id]);
+  res.json({ message: "can't find the shorturl" });
 });
 
 app.listen(port, function() {
